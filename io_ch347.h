06f116ff0ffd6ee58a5697277558fbfa14163737
DCZYewen <lzhallinone@outlook.com>
2024-11-04 00:30:38 +0800

    remove old files

--- io_ch347.h
+++
@@ -1,4 +0,0 @@
-int io_init(unsigned int index);
-int io_set_period(unsigned int index, unsigned int period);
-int io_scan(const unsigned char *TMS, const unsigned char *TDI, unsigned char *TDO, int bits);
-void io_close(void);
